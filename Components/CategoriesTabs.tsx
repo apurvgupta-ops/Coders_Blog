bfedaf4f34f3a897f3194bf9de578d238ddc8be7
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2023-03-16 23:10:30 +0530

    Active links

--- Components/CategoriesTabs.tsx
+++ Components/CategoriesTabs.tsx
@@ -1,5 +1,6 @@
 import { ICategory } from "@/Types";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface IPropType {
@@ -7,15 +8,36 @@ interface IPropType {
 }
 
 const CategoriesTabs = ({ categories }: IPropType) => {
+  const router = useRouter();
+
+  const isActiveLink = (category: ICategory) => {
+    return category.attributes.Slug === router.query.category;
+  };
+
   return (
-    <div className="border-b-2">
+    <div className="border-b">
       <ul className="flex gap-5 text-gray-500 font-medium  ">
-        <li>
+        <li
+          className={`${
+            router.pathname === "/"
+              ? "border-b-2 border-primary text-primary"
+              : "border-b-2 border-white text-gray-500"
+          }`}
+        >
           <Link href="#">Recent</Link>
         </li>
         {categories.map((category) => (
-          <li key={category.id}>
-            <Link href="#">{category.attributes.Title}</Link>
+          <li
+            key={category.id}
+            className={`${
+              isActiveLink(category)
+                ? "border-b-2 border-primary text-primary"
+                : "border-b-2 border-white text-gray-500"
+            }`}
+          >
+            <Link href={`/category/${category.attributes.Slug}`}>
+              {category.attributes.Title}
+            </Link>
           </li>
         ))}
       </ul>
